cf7eb3d2865bdce7890c5ea2cedfd6e3ef55bc73
Jose-Borges-Dev <joseborgesneto20@gmail.com>
2023-03-04 22:28:29 -0400

    Bug fixed
    Bug do nome das pastas inválidas pelo windows resolvidos.

--- index.py
+++ index.py
@@ -2,20 +2,24 @@ import requests
 from bs4 import BeautifulSoup
 import os
 import yt_dlp
-import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
-options = webdriver.ChromeOptions()
-options.add_argument('--disable-blink-features=AutomationControlled')
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-servico = Service(ChromeDriverManager().install())
-options.add_argument('--headless')
-options.add_argument("--log-level=3")
-navegador = webdriver.Chrome(service=servico, options=options)
-navegador.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
+try:
+    print("Preparando o ambiente...")
+    options = webdriver.ChromeOptions()
+    options.add_argument('--disable-blink-features=AutomationControlled')
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    servico = Service(ChromeDriverManager().install())
+    options.add_argument('--headless')
+    options.add_argument("--log-level=3")
+    navegador = webdriver.Chrome(service=servico, options=options)
+    navegador.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
+    os.system("cls || clear")
+except:
+    print("[ERRO] -> Instalação e configuração dos requisitos falhou, reporte!")
 
 def baixar_a_aula(ntemporada, episodio, link, anime):
     navegador.get(link)
@@ -26,6 +30,7 @@ def baixar_a_aula(ntemporada, episodio, link, anime):
     if os.path.exists(f"{anime}/Temporada {ntemporada}/{episodio}/{episodio}.mp4"):
         print(f'{episodio} da temporada {ntemporada} já baixado, pulando...')
     else:
+        os.system("cls || clear")
         ydl_opts = {"format": "best",
                     'retries': 9,
                     'fragment_retries': 9,
@@ -55,6 +60,25 @@ def main():
     soup = BeautifulSoup(r, 'html.parser')
     seasons = soup.find('div', {'id': 'seasons'})
     anime = soup.find('div', {'class', 'data'}).contents[1].text
+    if ':' in anime:
+        anime = anime.replace(":", "-")
+    if '\\' in anime:
+        anime = anime.replace("\\", "-")
+    if '/' in anime:
+        anime = anime.replace("/", "-")
+    if '|' in anime:
+        anime = anime.replace("|", "-")
+    if '>' in anime:
+        anime = anime.replace(">", "-")
+    if '<' in anime:
+        anime = anime.replace("<", "-")
+    if '*' in anime:
+        anime = anime.replace("*", "-")
+    if '"' in anime:
+        anime = anime.replace('"', "-")
+    if '?' in anime:
+        anime = anime.replace("?", "-")
+    print("Verificando... Aguarde!")
     pegar_temporada(seasons, anime)
 
 main()
